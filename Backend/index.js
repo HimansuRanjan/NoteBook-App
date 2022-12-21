f2b583e2bcf417e963555ca6c92efeca2f95ae6b
Himansu Ranjan Patra <mzzhimansu@gmail.com>
2022-12-21 20:59:34 +0530

    App Connected to Mongo

---
+++ Backend/index.js
@@ -0,0 +1,14 @@
+const connectToMongo = require('./db');
+const express = require("express");
+
+connectToMongo();
+
+const app = express();
+
+app.get('/', (req, res)=>{
+    res.send("Hello World");
+})
+
+app.listen(3000, ()=>{
+    console.log(`App running at http://localhost:3000`);
+})
